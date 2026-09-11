6885e6e578ad6f465b316f877cf5e3861df3a7de
qqqxxxrrr <14745310225@163.com>
2026-09-11 22:43:01 +0800

    chore: ignore electron dist-types build output

--- apps/electron/dist-types/preload/preload.d.ts
+++
@@ -1,8 +0,0 @@
-export interface AposApi {
-    init: () => Promise<unknown>;
-    setMode: (mode: "explore" | "ask" | "allow-all") => Promise<unknown>;
-    newSession: () => Promise<unknown>;
-    send: (text: string) => Promise<unknown>;
-    listFeatures: () => Promise<unknown>;
-    onAgentEvent: (cb: (evt: unknown) => void) => () => void;
-}
